cd582d934089b9f84eed74f20bf7e2de4a7d5eb8
Anish-Jha <anishj545@gmail.com>
2024-01-13 12:16:22 +0530

    file

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 const PORT = 3000; 
 
 app.use(express.json());
-app.use(cors({origin: "*"}));
+// app.use(cors({origin: "*"}));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
 });
 
 app.post('/upload', async (req, res) => {
