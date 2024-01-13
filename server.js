869df4eb818afab3d69f9a1287683e4f80e553a1
Anish-Jha <anishj545@gmail.com>
2024-01-13 12:17:52 +0530

    file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000; 
 
 app.use(express.json());
-// app.use(cors({origin: "*"}));
+app.use(cors({origin: "*"}));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
