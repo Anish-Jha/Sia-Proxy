c562478aa282e4127949602307af7d0f6189cb55
Anish-Jha <anishj545@gmail.com>
2024-01-13 12:21:58 +0530

    file

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require("cors");
+const cors = require('cors');
 const app = express();
-const PORT = 3000; 
+const PORT = 3000;
 
 app.use(express.json());
-// app.use(cors({origin: "*"}));
+app.use(cors()); // Enable CORS for all routes
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
+});
+
+// Handle preflight requests
+app.options('/upload', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'PUT');
+  res.send();
 });
 
 app.get('/', (req, res) => {
